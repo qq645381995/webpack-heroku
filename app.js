53c27aef63f2aeb664b942d95f28f174c9b80173
黄建峰 <645381995@qq.com>
2016-08-16 12:39:41 +0800

    'push'

--- app.js
+++ app.js
@@ -15,6 +15,40 @@ if (process.env.NODE_ENV !== 'production') {
     publicPath: config.output.publicPath
   }))
 }
+var express = require('express');
+var cheerio = require('cheerio');
+var superagent = require('superagent');
+
+app.get('/index', function (req, res, next) {
+    superagent.get('https://cnodejs.org/')
+        .end(function (err, sres) {
+            if (err) {
+                return next(err);
+            }
+            var $ = cheerio.load(sres.text);
+            var items = [];
+            //   $('#topic_list .topic_title').each(function (idx, element) {
+            //     var $element = $(element);
+            //     items.push({
+            //       title: $element.attr('title'),
+            //       href: $element.attr('href')
+            //     });
+            //   });
+            
+            $('#topic_list .cell').each(function (idx, element) {
+                var $element = $(element).find('.topic_title');
+                var $element2 = $(element).find('.last_active_time');
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href'),
+                    text: $element2.text()
+                });
+            });
+
+
+            res.send(items);
+        });
+});
 
 app.listen(port)
 console.log(`Listening at http://localhost:${port}`)
--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,43 @@
 import React from 'react'
-
+var superagent = require('superagent');
 export default class App extends React.Component {
-  render () {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      list: []
+    }
+  }
+
+  componentDidMount() {
+    // superagent.get('/index')
+    //   .end(function (err, sres) {
+    //     console.log(sres);
+    //   }
+    const _this = this;
+    superagent.get('/index')
+      .end(function (err, sres) {
+        if (err) {
+          return next(err);
+        }
+
+        _this.setState({
+          list: sres.body
+        })
+        console.log(sres.body);
+      });
+  }
+
+  render() {
+    console.log(111, this.state.list);
     return (
       <div>
         <h1>Change me</h1>
+        <ul>
+          {this.state.list.map((file, i) =>
+            <li key={i}>{file.title}</li>
+          ) }
+
+        </ul>
       </div>
     )
   }
