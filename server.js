ba133e3dac0db548ef9986a32efb8fa3366aad96
黄建峰 <645381995@qq.com>
2016-08-16 13:31:18 +0800

    '11'

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ module.exports = {
     const indexPath = path.join(__dirname, 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
 
-    // app.use('/public', publicPath)
+    app.use('/public', publicPath)
     app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
