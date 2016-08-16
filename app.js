f3633d91b641c8a4dd341fd24c45e630d2fcd9c5
黄建峰 <645381995@qq.com>
2016-08-16 13:37:54 +0800

    '11'

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ if (process.env.NODE_ENV !== 'production') {
 var express = require('express');
 var cheerio = require('cheerio');
 var superagent = require('superagent');
-
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.get('/index', function (req, res, next) {
     superagent.get('https://cnodejs.org/')
         .end(function (err, sres) {
