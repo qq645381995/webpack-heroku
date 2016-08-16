0c8928392e92990e193816d2a374417d4b5e4062
黄建峰 <645381995@qq.com>
2016-08-16 13:29:55 +0800

    '11'

--- app.js
+++ app.js
@@ -27,14 +27,6 @@ app.get('/index', function (req, res, next) {
             }
             var $ = cheerio.load(sres.text);
             var items = [];
-            //   $('#topic_list .topic_title').each(function (idx, element) {
-            //     var $element = $(element);
-            //     items.push({
-            //       title: $element.attr('title'),
-            //       href: $element.attr('href')
-            //     });
-            //   });
-            
             $('#topic_list .cell').each(function (idx, element) {
                 var $element = $(element).find('.topic_title');
                 var $element2 = $(element).find('.last_active_time');
